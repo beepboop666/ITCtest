6dae98aa1b8e02c93f5163248856343972319068
Данил Свиридов <linuxtux48@icloud.com>
2022-04-20 14:13:18 +0300

    Database turned on

--- ITCtest/settings.py
+++ ITCtest/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'ITCtest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#  DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.postgresql_psycopg2',
-#          'NAME': 'django_db',
-#          'USER': 'user_name',
-#          'PASSWORD': 'password',
-#          'HOST': '127.0.0.1',
-#          'PORT': '5432',
-#      }
-#  }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'user_name',  # TODO: replace db auth info
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
